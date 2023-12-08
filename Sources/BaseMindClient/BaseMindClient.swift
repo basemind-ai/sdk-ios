45a6a365f6caf2fc822b4eae841945f43503a186
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2023-12-08 16:47:24 +0100

    chore: updated proto build and genrated code

--- Sources/BaseMindClient/BaseMindClient.swift
+++ Sources/BaseMindClient/BaseMindClient.swift
@@ -1,8 +1,10 @@
 import BaseMindGateway
 import Foundation
 import GRPC
+import Logging
 
 enum BaseMindError: Error {
+    case missingToken
     case connectionFailure
 }
 
@@ -10,41 +12,65 @@ let DEFAULT_API_GATEWAY_ADDRESS = "gateway.basemind.ai"
 let DEFAULT_API_GATEWAY_PORT = 443
 
 public struct Options {
+    /* The gRPC server address. */
     var host: String = DEFAULT_API_GATEWAY_ADDRESS
+    /* The gRPC server port. */
     var port: Int = DEFAULT_API_GATEWAY_PORT
+    /* A flag dictating whether debug messages will be logged. */
     var debug: Bool = false
+    /* The ID of the prompt configuration to use.
+
+     This value is optional. If not provided, the default prompt configuration will be used.
+     */
     var promptConfigId: String?
-    var logger: Logger = .init(
-        subsystem: Bundle.main.bundleIdentifier!,
-        category: "BaseMindClient"
-    )
+    /* The logger instance to use.
+
+     Note: messages are logged only when 'debug' is set to true.
+     */
+    var logger: Logger = .init(label: "BaseMindClient")
 }
 
 public class BaseMindClient {
-    private let apiToken: String = ""
-    private let promptConfigId: String?
+    private let client: BaseMindGateway.Gateway_V1_APIGatewayServiceAsyncClient
+    private let apiToken: String
+    private let options: Options
 
     init(apiToken: String, options: Options = Options()) throws {
-        self.apiToken = apiToken
-
-        if options.debug {
-            options.logger.debug("initializing client")
+        if apiToken == "" {
+            throw BaseMindError.missingToken
         }
 
-        let eventLoop = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+        self.apiToken = apiToken
+        self.options = options
 
+        if self.options.debug {
+            self.options.logger.debug("initializing client")
+        }
+
+        let eventLoopGroup = PlatformSupport.makeEventLoopGroup(loopCount: 1)
         do {
             let channel = try GRPCChannelPool.with(
                 target: .host(options.host, port: options.port),
-                transportSecurity: .tls,
+                transportSecurity: .plaintext,
                 eventLoopGroup: eventLoopGroup
             )
+            client = BaseMindGateway.Gateway_V1_APIGatewayServiceAsyncClient(channel: channel)
+
+            if self.options.debug {
+                self.options.logger.debug("Successfully connected to BaseMind.AI server on \(options.host):\(options.port).")
+            }
         } catch {
-            if options.debug {
-                options.logger.debug("Failed to connect to BaseMind server on \(options.host):\(options.port). Error: \(error).")
+            if self.options.debug {
+                self.options.logger.debug("Failed to connect to the BaseMind.AI server on \(options.host):\(options.port). Error: \(error).")
             }
 
             throw BaseMindError.connectionFailure
         }
     }
+
+    func requestPrompt(templateVariables _: [String: String]? = nil) async -> BaseMindGateway.Gateway_V1_PromptResponse {
+        do {}
+    }
+
+    func requestStream(templateVariables _: [String: String]? = nil) async -> BaseMindGateway.Gateway_V1_StreamingPromptResponse {}
 }
